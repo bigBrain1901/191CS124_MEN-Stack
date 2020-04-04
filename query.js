b123b7cc99d47c924884bc9ee54eb0931ef63550
bigBrain1901 <asishaan.191cs124@nitk.edu.in>
2020-04-05 00:26:17 +0530

    Major refactoring done on server

---
+++ query.js
@@ -0,0 +1,78 @@
+//jshint esversion:6
+const mysql = require("mysql");
+const md5 = require("md5");
+
+//mysql connection parameters
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "27021973",
+    database: "irisrec"
+});
+
+//standard function to open a connection to the database
+con.connect()
+
+//this function executes a create query
+//returns 1 if successful, 0 otherwise
+function queryInsert(sql, cb) {
+    con.query(sql, (err, _) => {
+        cb((!err) ? 1 : 0);
+    });
+}
+
+//this function executes a read query
+//returns [objects] if successful, [] otherwise
+function querySelect(sql, callback) {
+    con.query(sql, (err, res) => {
+        callback((!err) ? res : []);
+    });
+}
+
+//this function prepares a read command
+//returns the command
+function makeSelect(table, condition, cb) {
+    querySelect("select * from " + table + ((condition != "") ? (" where " + condition) : "") + ";", (res) => {
+        cb(res);
+    });
+}
+
+//this function executes a read query
+//returns 1 if successful, 0 otherwise
+function queryUpdate(sql, cb) {
+    con.query(sql, (err, res) => {
+        cb((!err) ? 1 : 0);
+    });
+}
+
+//this function prepares a create command
+//returns the command
+function makeInsert(table, params, cb) {
+    let sql = "";
+    if (table == "items") {
+        sql = "insert into items (creatorid, name, description, starting_bid, deadline, contact, image, status, highest_bidder) values (";
+        sql += params.loginID + ",\"" + params.name + "\",\"" + params.description + "\"," + params.starting_bid;
+        sql += ",\"" + params.deadline + "\",";
+        sql += (params.contact != '') ? params.contact : "0";
+        sql += ",";
+        sql += "\"" + params.image + "\",\"N\", " + params.loginID + ");";
+    } else {
+        sql = "insert into users (rno, name, pwd, pno) values (\"" + params.rno + "\", \"";
+        sql += params.name + "\", \"" + md5(params.pwd) + "\", " + params.pno + ");";
+    }
+    queryInsert(sql, (res) => {
+        cb(res);
+    });
+}
+
+//this function prepares an update command
+//returns the command
+function makeUpdate(param, newValue, condition, cb) {
+    queryUpdate("update items set " + param + " = " + newValue + " where " + condition + ";", (res) => {
+        cb(res);
+    });
+}
+
+exports.makeSelection = makeSelect;
+exports.makeInsertion = makeInsert;
+exports.makeUpdation = makeUpdate;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+//jshint esversion:6
 //import section...
-const mysql = require("mysql");
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
@@ -7,172 +8,134 @@ const md5 = require("md5");
 const path = require("path");
 const multer = require("multer");
 const bodyParser = require("body-parser");
+const query = require(__dirname + "/query");
 
 //environment setup section...
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //app-variable
 var loginID = -1;
-let array = [];
-var auctionFlag = false;
+var flag = false;
 var msg = "";
 
-//mysql connection parameters
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "27021973",
-  database: "irisrec"
-});
-
-//multer storage parameters - image-upload handler
+//multer image-upload strategy
 var storage = multer.diskStorage({
   destination: "./public/uploads/",
   filename: function (_, file, cb) {
-    cb (null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
   }
 });
-
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype=='image/jpg' || file.mimetype=='image/jpeg' || file.mimetype=='image/png' || file.mimetype=='image/jpg') cb (null,true);
-  else cb (new alert("Invalid filetype..."), false);
-}
-
-
-//upload strategy for images
+const fileFilter = (_, file, cb) => {
+  if (file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/jpg') cb(null, true);
+  else cb(new encodeURI("/add"), false);
+};
 const upload = multer({
   storage: storage,
-  limits:{
-    fileSize: 1024*1024*5
+  limits: {
+    fileSize: 1024 * 1024 * 5
   },
   fileFilter: fileFilter
 });
+
 //---------------------------------------------------------------------------------------------
 //SERVER ROUTING STARTS NOW...
 
 //function for home route - login Page
 app.route("/")
-  .get(function(_,res) {
+  .get(function (_, res) {
     logout();
-    res.render("index",{type:"n", text:""});
+    res.render("index", {
+      type: "n",
+      text: ""
+    });
   })
-  .post(function(req,res) {
+  .post(function (req, res) {
     if (req.body.btn == "login") loginUser(req.body.rno, req.body.pwd, res);
-    else registerUser(req.body.rno, req.body.name, req. body.pwd, req.body.pno, res);
+    else registerUser(req.body.rno, req.body.name, req.body.pwd, req.body.pno, res);
   });
 
 //function for add route - Add Items for Auction Page
 app.route("/add")
-  .get(function(_,res) {
-    if (loggedIn()) res.render("addItem",{type:"n", text:""});
+  .get(function (_, res) {
+    if (loggedIn()) res.render("addItem", {
+      type: "n",
+      text: ""
+    });
     else res.redirect("/");
   })
-  .post(upload.single("image"), function(req,res) {
+  .post(upload.single("image"), function (req, res) {
     let params = {
       name: req.body.name,
       description: req.body.desc,
       starting_bid: req.body.starting_bid,
       deadline: req.body.deadline,
       contact: req.body.pno,
-      image: req.file.filename
+      image: req.file.filename,
+      loginID: loginID
     };
-    let sql = sqlInsert("items", params);
-    
-    con.connect(function(_) {
-      con.query(sql, function (error,_) {
-        if (!error) res.render ("addItem",{type:"s", text:"Your item has been added for auction!"});
-        else res.render("addItem",{type:"f", text:"Invalid entry detected. Please check and re-enter details."});
+    query.makeInsertion("items", params, (result) => {
+      if (result == 1) res.render("addItem", {
+        type: "s",
+        text: "Your item has been added for auction!"
       });
+      else {
+        fs.unlink(req.file.filename, (err) => {});
+        res.render("addItem", {
+          type: "f",
+          text: "Invalid entry detected. Please check and re-enter details."
+        });
+      }
     });
   });
 
 //function for auction route - Auction Dashboard Page
 app.route("/auction")
-  .get(function(_,res) {
+  .get(function (_, res) {
     if (loggedIn()) {
-      array = [];
-      let sql = "select * from items where deadline >= DATE(NOW());";
-      con.query(sql, function (error,result) {
-        if (!error && result.length>0) {
-          for (let i=0; i< result.length; i++) {
-            
-            let current_bid, highest_bidder;
-            if (result[i].current_bid == 0) {       
-              current_bid = result[i].starting_bid;
-              highest_bidder = "No bids made yet!";
-            } else {
-              current_bid = result[i].current_bid;
-              let string = "select * from users where id = " +result[i].highest_bidder+ ";";
-              con.connect(function(_) {
-                con.query(string, function (error,response) {
-                  if (!error) highest_bidder = response[0].name;                
-                  else res.render("auction",{type:"f", text:"Database management error. Contact an admin...", cards:[]});
-                });
-              });
-            }
-            
-            let object = {
-              id: result[i].id,
-              name: result[i].name,
-              image: "uploads/" + result[i].image,
-              currentBid: current_bid,
-              description: result[i].description.substring(0,110) + ((result[i].description.length<110)? "" : " ..."),
-              deadline: result[i].deadline,
-              highestBidder: highest_bidder
-            };
-            
-            array.push(object);
-          }
-          if (auctionFlag && msg!="") res.render("auction",{type:"s", text:"Your bid has been registered. Check dashboard for info.", cards: array});
-          else if (!auctionFlag && msg!="") res.render("auction",{type:"f", text:"Stop messing with the HTML!",cards: array});
-          else res.render("auction",{type:"n", text:"", cards:array});
-        }
-        else res.render("auction",{type:"f", text:"Noone has uploaded items yet. Be the first one!", cards:[]});
+      query.makeSelection("items", "", (result) => {
+        console.log(result);
+
+        if (result.length != 0) renderAuction(result, res);
+        else res.render("auction", {
+          type: "f",
+          text: "Noone has uploaded items yet. Be the first one!",
+          cards: []
+        });
       });
-    }
-    else res.redirect("/");
+    } else res.redirect("/");
   })
   .post();
 
 //route for updatig bidding status of items
-app.post("/bid/:key", function (req,res) {
+app.post("/bid/:key", function (req, res) {
   auctionFlag = false;
   msg = "";
-  let sql = "select * from items where id = " +req.params.key+ ";";
-  con.query(sql, function (error,result) {
-    if (!error) {
-      if (req.body.bid > result[0].current_bid) {
-        let string = sqlUpdate("current_bid",req.body.bid,"id = "+req.params.key);
-        con.query(string, function (_,_) {});
-        string = sqlUpdate("highest_bidder",loginID,"id = "+req.params.key);
-        con.query(string, function (_,_) {});
-        auctionFlag = true;
-        msg = "Your bid has been registered. Check dashboard for info.";
-        res.redirect("/auction");
-      } else {
-        auctionFlag = false;
-        msg = "Stop messing with the HTML!";
-        res.redirect("/auction");
-      } 
+  query.makeSelection("items", "id = " + req.params.key, (result) => {
+    if (req.body.bid > result[0].current_bid) {
+      query.makeUpdation("current_bid", req.body.bid, "id = " + req.params.key, (result) => {});
+      query.makeUpdation("highest_bidder", loginID, "id = " + req.params.key, (result) => {});
+      flag = true;
+      msg = "Your bid has been registered. Check dashboard for info.";
     } else {
       auctionFlag = false;
       msg = "Stop messing with the HTML!";
-      res.redirect("/auction");
     }
   });
+  res.redirect("/auction");
 });
 
 //port-on-system to listen on
 app.listen(3000);
 
-
 //---------------------------------------------------------------------------------------------
 //OTHER DEPENDENCY FUNCTIONS...
 
 function loggedIn() {
-  if (loginID!=-1) return true;
+  if (loginID != -1) return true;
   else return false;
 }
 
@@ -180,61 +143,72 @@ function logout() {
   loginID = -1;
 }
 
-function sqlUpdate (param, newValue, condition) {
-  //works only for items table
-  //construction of sql command
-  return "update items set " +param+ " = " +newValue+ " where " +condition+ ";";
-}
-
-function sqlInsert (table, params) {
-  //construction of sql command - looks ugly
-  let sql = "";
-  if (table == "items") {
-    sql = "insert into items (creatorid, name, description, starting_bid, deadline, contact, image, status) values (";
-    sql += loginID +",\""+ params.name +"\",\""+ params.description +"\","+ params.starting_bid;
-    sql += ",\""+ params.deadline +"\",";
-    sql += (params.contact != '') ? params.contact : "0";
-    sql += ",";
-    sql += "\""+ params.image +"\",\"N\");";
-  } else {
-    sql = "insert into users (rno, name, pwd, pno) values (\"" + params.rno + "\", \"";
-    sql += params.name + "\", \""+ md5(md5(params.pwd)) + "\", "+ params.pno + ");";
-  }
-  return sql;
-}
-
-function sqlSelect (table, condition) {
-  //construction of sql command
-  return "select * from " +table+ " where " +condition+ ";";
-}
-
-function loginUser(rno,pwd,res) {
-  con.connect(function(_) {
-    let sql = sqlSelect("users", "rno = \"" + rno + "\" and pwd = \"" + md5(md5(pwd)) + "\"");
-    con.query(sql, function (error,result) {
-      if (!error && result.length!=0) {
-        loginID = result[0].id;
-        res.redirect("/add");
-      } else res.render("index",{type:"f", text:"We were unable to log you in. Please check your credentials."});
+//this function checks and logs the user in
+function loginUser(rno, pwd, res) {
+  query.makeSelection("users", "rno = \"" + rno + "\" and pwd = \"" + md5(pwd) + "\"", (result) => {
+    if (result.length > 0) {
+      loginID = result[0].id;
+      res.redirect("/add");
+    } else res.render("index", {
+      type: "f",
+      text: "We were unable to log you in. Please check your credentials."
     });
   });
 }
 
-function registerUser(rno,name,pwd,pno,res) {
-  let result = sqlInsert("users", {
+//this function adds a new user to database
+function registerUser(rno, name, pwd, pno, res) {
+  query.makeInsertion("users", {
     rno: rno,
     name: name,
     pwd: pwd,
     pno: pno
-  });
-  con.connect(function(_) {
-    con.query(result, function (error,_) {
-      if (!error) res.render("index",{type:"s", text:"Your account has been created. You can now login."});
-      else res.render("index",{type:"f", text:"This account already exists or your data is invalid."});
+  }, (result) => {
+    if (result == 1) res.render("index", {
+      type: "s",
+      text: "Your account has been created. You can now login."
+    });
+    else res.render("index", {
+      type: "f",
+      text: "This account already exists or your data is invalid."
     });
   });
 }
 
-function renderAuction() {
-  
+//breaking down complex page render...
+function renderAuction(result, res) {
+  prepareArray(result, (array) => {
+    if (flag && msg != "") res.render("auction", {
+      type: "s",
+      text: "Your bid has been registered. Check dashboard for info.",
+      cards: array
+    });
+    else if (!flag && msg != "") res.render("auction", {
+      type: "f",
+      text: "Stop messing with the HTML!",
+      cards: array
+    });
+    else res.render("auction", {
+      type: "n",
+      text: "",
+      cards: array
+    });
+  });
+}
+
+//breaking down complex page render... contd...
+function prepareArray(result, cb) {
+  let array = [];
+  for (let i = 0; i < result.length; i++) {
+    let object = {
+      id: result[i].id,
+      name: result[i].name,
+      image: "uploads/" + result[i].image,
+      currentBid: (result[i].current_bid == 0) ? result[i].starting_bid : result[i].current_bid,
+      description: result[i].description.substring(0, 90) + ((result[i].description.length < 90) ? "" : " ..."),
+      deadline: result[i].deadline,
+    };
+    array.push(object);
+  }
+  cb(array);
 }
